c9c9fd9eab2bc7ddeb3793db1adc23cae7b55033
Ben Gottlieb <ben@standalone.com>
2021-10-15 04:50:32 -0500

    updated requirements

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/bengottlieb/crossplatformkit.git", from: "1.0.3"),
-	   .package(url: "https://github.com/bengottlieb/suite.git", from: "0.10.54")
+	   .package(url: "https://github.com/bengottlieb/suite.git", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
